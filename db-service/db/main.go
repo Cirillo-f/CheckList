1b9f04d9cfb3cf6c877aad13cdfc6471dd1b7cfe
Cirillo-F <fairbrother06@bk.ru>
2025-01-17 01:59:38 +0300

    change in main.go in db-service

--- db-service/db/main.go
+++ db-service/db/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	dbAPP := chi.NewRouter()
+
+	log.Println("DB-service is listening on http://localhost:8081")
+	err := http.ListenAndServe(":8081", dbAPP)
+	if err != nil {
+		log.Fatal("[ERROR]:", err)
+	}
+}
