184b630ca90e2e9fdc65d1aca9719c382589fc5b
Cirillo-F <fairbrother06@bk.ru>
2025-02-01 02:53:49 +0300

    new logg feb1

--- db-service/models/databaseStruct.go
+++ db-service/models/databaseStruct.go
@@ -1,5 +1,6 @@
 package models
 
+// Структура задачи
 type Task struct {
 	ID          string `json:"id"`
 	Title       string `json:"title"`
@@ -7,20 +8,24 @@ type Task struct {
 	Status      string `json:"status"`
 }
 
+// Структура сообщения
 type MessageNewTS struct {
 	Text  string `json:"text"`
 	NewTS string `json:"newtask"` //New task/status
 }
 
+// Завершить задачу
 type DoneStatus struct {
 	ID        int    `json:"id"`
 	NewStatus string `json:"newstatus"`
 }
 
+// ID задачи которую мы будем удалять
 type DeleteIDTask struct {
 	ID int `json:"id"`
 }
 
+// Сообщение о том, какую задачу мы удалили
 type DeleteTaskMessage struct {
 	Text string `json:"text"`
 }
